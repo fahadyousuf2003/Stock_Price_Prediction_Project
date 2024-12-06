1f58f71ac7621f2ce046bd06716c887a3630d0cc
Fahad <fahadyousuf344@gmail.com>
2024-12-06 21:43:57 +0500

    Github

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def predict():
         return render_template('error.html', error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
